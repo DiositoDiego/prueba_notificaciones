47f9b6eb35ffd5340c89b293bb435e4a545d5f9c
DiositoDiego <20213tn096@utez.edu.mx>
2024-11-25 11:24:25 -0600

    [UPDATE] archivos manifest y delegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -16,11 +16,6 @@ import flutter_local_notifications
     }
 
     GeneratedPluginRegistrant.register(with: self)
-
-      if #available(iOS 10.0, *){
-        UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-      }
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
